9cc5d33aa21dcb62adf99ae6606aa47597665c1f
Cody Rioux <cody.rioux@gmail.com>
2014-03-20 11:56:30 -0600

    Added an ngram tfidf vectorizer.

--- src/reaper/features/tfidf.clj
+++ src/reaper/features/tfidf.clj
@@ -3,12 +3,10 @@
     [reaper.util :as util]
     [reaper.tools.stop-words :refer [tokens->stop-words]]
     [reaper.tools.porter-stemmer :refer [tokens->stemmed-tokens]]
-    [reaper.tools.tfidf :refer [corpus->tfidf-model tfidf-model->top-n-terms]]))
+    [reaper.tools.tfidf :refer [corpus->tfidf-model corpus->ngram-tfidf-model tfidf-model->top-n-terms]]))
 
 (defn make-tfidf-vectorizer
-  "Produces a fn that takes a string and computes a tf*idf vector representation.
-If passed a query seq of tokens it will produce a vector with n elements, and
-|query| additional elements."
+  "Produces a fn that takes a string and computes a tf*idf vector representation."
   [corpus n & {:keys [remove-stopwords
                       stem]
                :or {remove-stopwords false
@@ -28,3 +26,27 @@ If passed a query seq of tokens it will produce a vector with n elements, and
         (reduce #(assoc %1 (w->idx %2) (get tfidf-model %2 0))
                 (vec (repeat n 0))
                 (filter #(some #{%} (keys w->idx)) s))))))
+
+(defn make-ngram-tfidf-vectorizer
+  "Produces a fn that takes a string and computes an ngram tf*idf vector representation."
+  [corpus n m & {:keys [remove-stopwords
+                      stem]
+               :or {remove-stopwords false
+                    stem false}}]
+  (let
+    [ngram-tfidf-model (corpus->ngram-tfidf-model
+                         corpus n
+                         :remove-stopwords remove-stopwords
+                         :stem stem)
+     top-n-terms (tfidf-model->top-n-terms ngram-tfidf-model m)
+     w->idx (zipmap top-n-terms (range m))]
+    (fn [s]
+      (let
+        [s (util/tokenize-lower s)
+         s (if remove-stopwords (tokens->stop-words s) s)
+         s (if stem (tokens->stemmed-tokens s) s)
+         s (partition n 1 s)]
+        (reduce #(assoc %1 (w->idx %2) (get ngram-tfidf-model %2 0))
+                (vec (repeat m 0))
+                (filter #(some #{%} (keys w->idx)) s))))))
+
