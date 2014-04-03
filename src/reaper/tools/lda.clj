6ee26332c859ac0fb33def27d0ede5a2e8728135
Cody Rioux <cody.rioux@gmail.com>
2014-04-03 12:04:18 -0600

    Added LDA functionality for a vectorizer.

--- src/reaper/tools/lda.clj
+++ src/reaper/tools/lda.clj
@@ -1,26 +1,87 @@
 (ns reaper.tools.lda
-  "A wrapper over the mallet-lda wrapper. Latent Dirichlet
-   Allocation for topic modelling.
-   
-   This namespace exists primarily to draw users attention to the LDA
-   functionality implemented by mallet-lda, as well as to implement utility
-   functions associated with LDA.
-   
-   https://github.com/marcliberatore/mallet-lda"
-  (:require [mallet-lda.core :as lda]
-            [reaper.util :as util]))
+  "A wrapper over the mallet library. Latent Dirichlet
+   Allocation for topic modelling."
+  (:import (cc.mallet.types InstanceList Instance))
+  (:import (cc.mallet.pipe Pipe SerialPipes CharSequence2TokenSequence TokenSequence2FeatureSequence))
+  (:import (cc.mallet.topics ParallelTopicModel))
+  (:require [reaper.util :as util]))
 
-(def lda lda)
+(defn- document-to-instance
+  "Convert a document ID and a text string to a MALLET instance"
+  [[documentid text]]
+  (Instance. text "nolabel" documentid nil))
 
-(defn- corpus->lda-format
+(defn- get-instance-list
+  "Convert (documentID, text) map to MALLET InstanceList"
+  [documentmap]
+  (let [pipes (new SerialPipes
+                   [(new CharSequence2TokenSequence #"\S+")
+                    (new TokenSequence2FeatureSequence)])]
+    (doto (new InstanceList pipes)
+      (.addThruPipe (.iterator (map document-to-instance documentmap))))))
+
+(defn- get-feature-sequence
+  [documentmap]
+  (let [pipes (new SerialPipes
+                   [(new CharSequence2TokenSequence #"\S+")
+                    (new TokenSequence2FeatureSequence)])]
+    (doto (new InstanceList pipes)
+      (.addThruPipe (.iterator (map document-to-instance documentmap))))))
+
+(defn run-lda
+  "Train a ParallelTopicModel from a given InstanceList with named
+   arguments for all LDA parameters"
+  [instancelist & {:keys [T numiter topwords
+                          showinterval numthreads
+                          optinterval optburnin
+                          usesymmetricalpha alpha beta
+                          thetathresh thetamax]
+                   :or {T 50 numiter 500
+                        topwords 20 showinterval 50
+                        numthreads
+                        (dec (.availableProcessors
+                               (Runtime/getRuntime)))
+                        optinterval 25 optburnin 200
+                        usesymmetricalpha false
+                        alpha 50.0 beta 0.01
+                        thetathresh 0.0 thetamax -1}}]
+  (doto (new ParallelTopicModel T alpha beta)
+    (.addInstances instancelist)
+    (.setTopicDisplay showinterval topwords)
+    (.setNumIterations numiter)
+    (.setOptimizeInterval optinterval)
+    (.setBurninPeriod optburnin)
+    (.setSymmetricAlpha usesymmetricalpha)
+    (.setNumThreads numthreads)
+    (.estimate)))
+
+(defn corpus->instances
   [corpus]
-  (map #(vec [%1 (flatten (map util/tokenizer %2))])
-       (range 1 (inc (count corpus))) corpus))
+  (let
+    [docs (zipmap (range (count corpus)) (map #(clojure.string/join " " %) corpus))]
+    (get-instance-list docs)))
+
+(defn lda->vectorizer
+  [lda-model & {:keys [numiter
+                       burnin]
+                :or {numiter 500
+                     burnin 200}}]
+  (let
+    [inferencer (.getInferencer lda-model)]
+    (fn
+      [sentence]
+      (.getSampledDistribution inferencer 
+                               (first (get-instance-list {1 sentence}))
+                               numiter numiter burnin))))
 
 (defn corpus->lda
-  [corpus n-topics]
-  (->>
-    corpus
-    corpus->lda-format
-    lda/make-instance-list
-    (lda :num-topics n-topics)))
+  "Creates an estimated ParallelTopicModel from a REAPER corpus."
+  [corpus T]
+  (run-lda (corpus->instances corpus) :T T))
+
+(defn corpus->lda-vectorizer
+  "Creates an LDA vectorizer from corpus with T topics."
+  [corpus & {:keys [T] :or {T 100}}]
+  (let
+    [lda (corpus->lda corpus T)]
+    (lda->vectorizer lda)))
